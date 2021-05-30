35a629edc056557384940cd3022795e6013eddda
JuanBarrosPazos <juanbarrospazos@hotmail.es>
2021-05-30 11:24:26 +0200

    Juancho

--- Admin_Modulo/Inclu/Inclu_Menu_00.php
+++ Admin_Modulo/Inclu/Inclu_Menu_00.php
@@ -77,34 +77,50 @@ require 'Inclu/misdatos.php';
  }
  </script>
 	
-<!--	
+<!--	-->	
 <style>
 /* INICIO ESTILOS VENTANA FLOTANTE
 	JUANBARROSPAZOS.BLOGSPOT*/
 
+@media screen and (max-width:740px){
+	#ventana-flotante { top: 40% !important;
+						left: 18% !important;
+							}
+					}
+@media screen and (max-width:440px){
+	#ventana-flotante { width: 98.0vw !important;  /* Ancho de la ventana */
+						height: auto;  /* Alto de la ventana */
+						margin-right: 0.5% !important;
+						padding-bottom: 16px;
+						top: 28% !important;
+						left: 0.5% !important;
+							}
+					}
+
 #ventana-flotante {
-width: 400px;  /* Ancho de la ventana */
-height: 80px;  /* Alto de la ventana */
+width: 450px;  /* Ancho de la ventana */
+height: 88px;  /* Alto de la ventana */
 background: #E4F1F1;  /* Color de fondo */
 position: fixed;
-top: 38%;
-left: 16%;
-margin-left: -180px;
-border: 1px solid #408080;  /* Borde de la ventana */
+top: 34%;
+left: 32%;
+/* margin-left: -180px; */
+border: 2px solid #408080;  /* Borde de la ventana */
+border-radius: 12px;
 box-shadow: 0 5px 25px rgba(0,0,0,.1);  /* Sombra */
 z-index:999;
 }
 #ventana-flotante .cerrar {
 float: right;
-border-bottom: 1px solid #408080;
-border-left: 1px solid #408080;
+border: 2px solid #408080;
+border-radius: 8px;
 color: #990000;
 background: #E4F1F1;
 line-height: 17px;
 text-decoration: none;
 padding: 0px 14px;
+margin: 2px;
 font-family: Arial;
-border-radius: 0 0 0 5px;
 box-shadow: -1px 1px white;
 font-size: 18px;
 -webkit-transition: .3s;
@@ -114,71 +130,71 @@ font-size: 18px;
 }
 #ventana-flotante .cerrar:hover {
 background: #990000;
-color: white;
+border: 2px solid #990000;
+border-radius: 8px;
+color: #fff !important;
 text-decoration: none;
-text-shadow: -1px -1px #990000;;
-border-bottom: 1px solid #990000;;
-border-left: 1px solid #990000;;
+text-shadow: -1px -1px #990000;
 }
 #ventana-flotante .contenido {
 padding: 15px;
 box-shadow: inset 1px 1px white;
-/*background: #deffc4;   Fondo del mensaje */
-/*border: 1px solid #9eff9e;   Borde del mensaje */
 font-size: 18px;  /* Tamaño del texto del mensaje */
-color: #555;  /* Color del texto del mensaje */
+font-weight: bold;
+color: #408080;  /* Color del texto del mensaje */
 text-shadow: 1px 1px white;
 margin: 0 auto;
-border-radius: 4px;
+border:none;
+border-radius: 12px;
 }
 #ventana-flotante a {
-	color: #666;
+	color: #408080 !important;
 	text-decoration: none;
 }
 #ventana-flotante a:hover {
-	color: #990000;
+	color: #990000 !important;
 	text-decoration: none;
 }
-
 .oculto {-webkit-transition:1s;-moz-transition:1s;-o-transition:1s;-ms-transition:1s;opacity:0;-ms-opacity:0;-moz-opacity:0;visibility:hidden;}
 
 /* FIN ESTILOS VENTANA FLOTANTE*/
 </style>
--->	
-<!-- 
-	
-<script type="text/javascript">
-	 
-	setTimeout("window.self.close();", 3000);
-	
-</script>
 
--->	
+<!-- CIERRO AUTO LA VENTANA DESPUES DE 3 SEGUNDOS	
+	
+	<script type="text/javascript">
+		
+		setTimeout("window.self.close();", 3000);
+		
+	</script>
+
+ -->
+
 	
 </head>
 	
 <body topmargin="0" onload="hora()">
 
 	
-<!--
+<!--	-->
 	<div id='ventana-flotante'>
    <a class='cerrar' href='javascript:void(0);' onclick='document.getElementById(&apos;ventana-flotante&apos;).className = &apos;oculto&apos;'>X</a>
--->
+
         <!--[if IE]>
         <style>
         .oculto {display:none}
         </style>
         <![endif]-->
-<!-- 
-	<div class='contenido' align="center" style="padding-top:22px">
+<!--	 -->
+	<div class='contenido' style="text-align:center; padding-top:22px">
        				DOWNLOAD THIS APP FREE AND MORE IN:
             <br/>
-<a href="http://juanbarrospazos.blogspot.com.es/" target="_blank" style="text-decoration:none" onclick='document.getElementById(&apos;ventana-flotante&apos;).className = &apos;oculto&apos;'>	
+			<a href="http://juanbarrospazos.blogspot.com.es/" target="_blank" >	
   				http://juanbarrospazos.blogspot.com.es/
             </a>
        </div>
-</div>
--->
+	</div>
+
 	<div id="Conte">
   <div id="head"> 
   			<span style="font-size:18px">
--- Admin_Modulo/Inclu_MInd/Master_Index_Header.php
+++ Admin_Modulo/Inclu_MInd/Master_Index_Header.php
@@ -1,7 +1,8 @@
 <?php
-global $usertitle;
+
 //$a = $_SESSION['Nombre'][0]." ".$_SESSION['Apellidos'];
 $a = $_SESSION['Nombre']." ".$_SESSION['Apellidos'];
+global $usertitle;
 $usertitle = substr($a,0,18);
 print ("
 <div style='clear:both'></div>
@@ -14,18 +15,19 @@ print ("
 
 	<header style='text-align:center'>
     <!--    -->
-    <img src='".$rutaindex."Users/".$_SESSION['ref']."/img_admin/".$_SESSION['myimg']."' class='imgtitle' />
-    
-    ".$usertitle."</br>
-    ".$niv."</br>
-        <a href='#'>
+    <img class='imgtitle' src='".$rutaindex."Users/".$_SESSION['ref']."/img_admin/".$_SESSION['myimg']."' />
+    <div class='ocultahead'>
+        ".$usertitle."</br>
+        ".$niv."</br>
+    </div>
+        <a href='#' class='ocultahead'>
             <form name='cerrar' action='".$rutaadmin."mcgexit.php' method='post'>
                 <input type='submit' value='CLOSE SESSION'  style='margin-top:2px;' class='botonverde' />
                 <input type='hidden' name='cerrar' value=1 />
             </form>
         </a>
 
- <a href='#'><i class='ic icoh'></i>
+ <a href='#'><i class='ic icoh ocultahead'></i>
 		<span style='color:#FFFFFF;vertical-align:middle'>MENU APP</span>
  </a>
     </header>
